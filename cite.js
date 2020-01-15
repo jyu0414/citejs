3403118edd74358fcffb24f5ed038fe156417c6d
Yuji Sasaki <jyu0414@gmail.com>
2020-01-15 09:59:42 +0900

    ref sample update

--- cite.js
+++ cite.js
@@ -17,6 +17,7 @@ class Article {
 
 class Bibtex {
 
+    //Change Display Method
     showIn(e) {
         let html = "";
         if (e == "default") {
@@ -37,10 +38,10 @@ class Bibtex {
         document.getElementById("table1").innerHTML = html
     }
 
-    loaded(){
+    _loaded(){
         if ((this.httpObj.readyState == 4) && (this.httpObj.status == 200)) {
             this.resp = this.httpObj.responseText;
-            let r = /(@)([a-z]+)\{([^,]+),(([^=\}]+)(=)[\s]*[\{"']([^\}"']*)[\}"']{1}[,]?)+/g;
+            let r = /(@)([a-zA-Z]+)\{([^,]+),(([^=\}]+)(=)[\s]*[\{"']([^\}"']*)[\}"']{1}[,]?)+/g;
             this.articles = [];
             let m;
             while ((m = r.exec(this.resp)) != null) {
@@ -56,7 +57,7 @@ class Bibtex {
         this.settings = settings;
         this.httpObj = new XMLHttpRequest();
         this.articles = [];
-        this.httpObj.onreadystatechange = this.loaded.bind(this);
+        this.httpObj.onreadystatechange = this._loaded.bind(this);
         if (this.httpObj) {
             this.httpObj.open("GET", file, true);
             this.httpObj.send(null);
